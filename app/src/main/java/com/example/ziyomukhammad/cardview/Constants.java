48e798c714f1c20c4ae5591d43f2edfd3a1e7b9b
Nuruddin <nuruddin.iminohunov@gmail.com>
2017-08-03 23:14:39 +0900

    Constatnts

---
+++ app/src/main/java/com/example/ziyomukhammad/cardview/Constants.java
@@ -0,0 +1,10 @@
+package com.example.ziyomukhammad.cardview;
+
+/**
+ * Created by nuruddin on 8/3/17.
+ */
+
+public  final class Constants {
+    public static final String FIREBASE_CHILD_NOTES = "notes";
+    public static final String FIREBASE_CHILD_USERS = "users";
+}
--- app/src/main/java/com/example/ziyomukhammad/cardview/MainActivity.java
+++ app/src/main/java/com/example/ziyomukhammad/cardview/MainActivity.java
@@ -6,10 +6,13 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DatabaseReference mNotesReference;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -24,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNotesReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_NOTES);
+
+
+
+
+
+
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
         mRecyclerView.setHasFixedSize(true);
