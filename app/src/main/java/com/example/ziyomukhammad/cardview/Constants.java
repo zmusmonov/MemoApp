9320151aa4893f30e706949b6cc42fc4de038394
ZiyoMuhammad <zm.usmonov@gmail.com>
2017-08-06 19:43:49 +0900

    commitde

--- app/src/main/java/com/example/ziyomukhammad/cardview/Constants.java
+++ app/src/main/java/com/example/ziyomukhammad/cardview/Constants.java
@@ -4,6 +4,10 @@ package com.example.ziyomukhammad.cardview;
  * Created by nuruddin on 8/3/17.
  */
 
+//Constant string for class names of firabase database
+//and other unchanged parameters
+
+
 public  final class Constants {
     public static final String FIREBASE_CHILD_NOTES = "notes";
     public static final String FIREBASE_CHILD_USERS = "users";
